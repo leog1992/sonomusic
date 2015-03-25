9d05b9c83f7fad5c55b9b2056074c24d26f462ea
pc <pc@ATDE.home>
2015-03-25 13:01:43 -0500

    Merge origin/master
    Conflicts:
    	trunk/sonomusic/src/Clases/Cl_Conectar.java

--- trunk/sonomusic/src/Clases/Cl_Usuario.java
+++ trunk/sonomusic/src/Clases/Cl_Usuario.java
@@ -14,6 +14,19 @@ public class Cl_Usuario {
     private String nick;
     private String contra;
     private String est = "1";
+    private String per_panel;
+    private String per_menu;
+    private String per_report;
+    private String per_venta;
+    private String per_adelanto;
+    private String per_coti_cliente;
+    private String per_comp_servicio;
+    private String per_conf_documentos;
+    private String per_caja_chica;
+    private String per_cierre_caja;
+    private String per_notas;
+    private String per_depositos;
+    
 
     public Cl_Usuario() {
     }
@@ -42,4 +55,101 @@ public class Cl_Usuario {
         this.est = est;
     }
 
+    public String getPer_panel() {
+        return per_panel;
+    }
+
+    public void setPer_panel(String per_panel) {
+        this.per_panel = per_panel;
+    }
+
+    public String getPer_menu() {
+        return per_menu;
+    }
+
+    public void setPer_menu(String per_menu) {
+        this.per_menu = per_menu;
+    }
+
+    public String getPer_report() {
+        return per_report;
+    }
+
+    public void setPer_report(String per_report) {
+        this.per_report = per_report;
+    }
+
+    public String getPer_venta() {
+        return per_venta;
+    }
+
+    public void setPer_venta(String per_venta) {
+        this.per_venta = per_venta;
+    }
+
+    public String getPer_adelanto() {
+        return per_adelanto;
+    }
+
+    public void setPer_adelanto(String per_adelanto) {
+        this.per_adelanto = per_adelanto;
+    }
+
+    public String getPer_coti_cliente() {
+        return per_coti_cliente;
+    }
+
+    public void setPer_coti_cliente(String per_coti_cliente) {
+        this.per_coti_cliente = per_coti_cliente;
+    }
+
+    public String getPer_comp_servicio() {
+        return per_comp_servicio;
+    }
+
+    public void setPer_comp_servicio(String per_comp_servicio) {
+        this.per_comp_servicio = per_comp_servicio;
+    }
+
+    public String getPer_conf_documentos() {
+        return per_conf_documentos;
+    }
+
+    public void setPer_conf_documentos(String per_conf_documentos) {
+        this.per_conf_documentos = per_conf_documentos;
+    }
+
+    public String getPer_caja_chica() {
+        return per_caja_chica;
+    }
+
+    public void setPer_caja_chica(String per_caja_chica) {
+        this.per_caja_chica = per_caja_chica;
+    }
+
+    public String getPer_cierre_caja() {
+        return per_cierre_caja;
+    }
+
+    public void setPer_cierre_caja(String per_cierre_caja) {
+        this.per_cierre_caja = per_cierre_caja;
+    }
+
+    public String getPer_notas() {
+        return per_notas;
+    }
+
+    public void setPer_notas(String per_notas) {
+        this.per_notas = per_notas;
+    }
+
+    public String getPer_depositos() {
+        return per_depositos;
+    }
+
+    public void setPer_depositos(String per_depositos) {
+        this.per_depositos = per_depositos;
+    }
+
+    
 }
