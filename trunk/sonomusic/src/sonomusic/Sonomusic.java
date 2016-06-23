c0b016727b2efd1794f086c62c75f2f0556138d4
SONOMUSIC <SONOMUSIC@SONOMUSICIMPORT>
2016-06-23 18:47:15 -0500

    23/06/2016

--- trunk/sonomusic/src/sonomusic/Sonomusic.java
+++ trunk/sonomusic/src/sonomusic/Sonomusic.java
@@ -15,21 +15,20 @@ import javax.swing.JOptionPane;
  */
 public class Sonomusic {
 
-    private static ServerSocket SERVER_SOCKET;
-
+//    private static ServerSocket SERVER_SOCKET;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         frm_login loader = new frm_login();
-        try {
-            SERVER_SOCKET = new ServerSocket(1334);
-            loader.setVisible(true);
-        } catch (IOException x) {
-            System.out.print(x);
-            System.out.println("Otra instancia de la aplicación se está ejecutando...");
-            JOptionPane.showMessageDialog(null, "La Aplicacion ya se esta ejecutando.");
-            System.exit(0);
-        }
+//        try {
+//            SERVER_SOCKET = new ServerSocket(1338);
+        loader.setVisible(true);
+//        } catch (IOException x) {
+//            System.out.print(x);
+//            System.out.println("Otra instancia de la aplicación se está ejecutando...");
+//            JOptionPane.showMessageDialog(null, "La Aplicacion ya se esta ejecutando.");
+//            System.exit(0);
+//        }
     }
 }
